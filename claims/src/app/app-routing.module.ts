343328a59df367fb911c8c3d9bcc3265283367d4
ustjavafsdb216 <235749@ust-global.com>
2022-11-19 11:58:53 +0530

    login+hospital

--- claims/src/app/app-routing.module.ts
+++ claims/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HospitalPageComponent } from './hospital-page/hospital-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path:"hospital-page", component:HospitalPageComponent},
+    {path:"login-page",component:LoginPageComponent}
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+//routes
